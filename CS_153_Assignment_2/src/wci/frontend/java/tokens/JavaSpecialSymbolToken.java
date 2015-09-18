ad6d2132f6120e03e26da2550cb53859a75df659
Chiu350 <cheungfaat.chiu@sjsu.edu>
2015-09-18 16:25:13 -0700

    Update QuestionMark

--- CS_153_Assignment_2/src/wci/frontend/java/tokens/JavaSpecialSymbolToken.java
+++ CS_153_Assignment_2/src/wci/frontend/java/tokens/JavaSpecialSymbolToken.java
@@ -37,7 +37,7 @@ public class JavaSpecialSymbolToken extends JavaToken
             case '+':  case '-':  case '*':  case '/':  case ',':
             case ';':  case '\'': case '=':  case '(':  case ')':
             case '[':  case ']':  case '{':  case '}':  case '^': 
-            case '%':  case '|':  case '&': {
+            case '%':  case '|':  case '&':  case '?':{
                 nextChar();  // consume character
                 break;
             }
